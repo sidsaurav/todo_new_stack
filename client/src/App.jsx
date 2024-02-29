d770d62ca07a420dded698872e45d04c8f4ab907
Siddharth Saurav <sid.saurav11@gmail.com>
2024-02-29 23:33:10 +0530

    add task react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,24 @@ function App() {
     return data
   }
 
+  const postData = async () => {
+    let res = await fetch('http://localhost:5000/todo', {
+      method: 'POST',
+      body: JSON.stringify({ title: title, description: desc }),
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+    if (res.ok) {
+      setTodo((todo) => {
+        return [...todo, { title: title, description: desc }]
+      })
+    }
+  }
+
   const [todo, setTodo] = useState([])
+  const [title, setTitle] = useState('')
+  const [desc, setDesc] = useState('')
 
   useEffect(() => {
     fetchData().then((d) => {
@@ -32,14 +49,24 @@ function App() {
         placeholder='enter title'
         type='text'
         style={{ padding: '4px', width: '300px' }}
+        onChange={(e) => {
+          setTitle(e.target.value)
+        }}
+        value={title}
       />
 
       <input
         placeholder='enter description'
         type='text'
         style={{ padding: '4px', width: '300px' }}
+        onChange={(e) => {
+          setDesc(e.target.value)
+        }}
+        value={desc}
       />
-      <button style={{ padding: '4px', width: '300px' }}>Submit</button>
+      <button style={{ padding: '4px', width: '300px' }} onClick={postData}>
+        Submit
+      </button>
       <Todo data={todo}></Todo>
     </div>
   )
